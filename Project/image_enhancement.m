75e8cfb81297008685cd554398fb11ccd27884f5
edwardtoday <edwardtoday@gmail.com>
2012-04-08 16:50:49 +0800

    Correct pasted comments in image_enhancement().

--- Project/image_enhancement.m
+++ Project/image_enhancement.m
@@ -17,5 +17,5 @@ if(isempty(image_enhanced))
     dataset_enhanced=uint8(dataset); % Convert to unsigned 8 bit numbers to save memory. 
     toc;
 end
-image_enhanced=1;  % Set 'loaded' to aviod loading the database again. 
+image_enhanced=1;  % Set 'image_enhanced' to aviod applying the enhancement again. 
 out=dataset_enhanced;
