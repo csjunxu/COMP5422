6490597f56b68be903ee3d97c46cd330ae2d7879
edwardtoday <edwardtoday@gmail.com>
2012-04-08 16:52:19 +0800

    Fix typo avoid -> avoid.

--- Project/image_enhancement.m
+++ Project/image_enhancement.m
@@ -17,5 +17,5 @@ if(isempty(image_enhanced))
     dataset_enhanced=uint8(dataset); % Convert to unsigned 8 bit numbers to save memory. 
     toc;
 end
-image_enhanced=1;  % Set 'image_enhanced' to aviod applying the enhancement again. 
+image_enhanced=1;  % Set 'image_enhanced' to avoid applying the enhancement again. 
 out=dataset_enhanced;
